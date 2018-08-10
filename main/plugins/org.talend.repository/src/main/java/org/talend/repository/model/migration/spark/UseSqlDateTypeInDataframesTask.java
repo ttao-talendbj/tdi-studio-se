a9a23fef910545ecf247df90df7dc0616a60b34b
kcoepeau <kcoepeau@talend.com>
2018-08-10 11:46:56 +0200

    fix(TBD-7495): Add checkbox to transform date type in timestamp

--- main/plugins/org.talend.repository/src/main/java/org/talend/repository/model/migration/spark/UseSqlDateTypeInDataframesTask.java
+++ main/plugins/org.talend.repository/src/main/java/org/talend/repository/model/migration/spark/UseSqlDateTypeInDataframesTask.java
@@ -10,7 +10,7 @@
 // 9 rue Pages 92150 Suresnes, France
 //
 // ============================================================================
-package org.talend.repository.model.migration;
+package org.talend.repository.model.migration.spark;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -18,9 +18,6 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.List;
 
-import org.eclipse.emf.common.util.EList;
-import org.talend.commons.exception.PersistenceException;
-import org.talend.commons.ui.runtime.exception.ExceptionHandler;
 import org.talend.core.model.components.ComponentUtilities;
 import org.talend.core.model.components.ModifyComponentsAction;
 import org.talend.core.model.components.conversions.IComponentConversion;
@@ -28,67 +25,72 @@ import org.talend.core.model.components.filters.NameComponentFilter;
 import org.talend.core.model.migration.AbstractJobMigrationTask;
 import org.talend.core.model.properties.Item;
 import org.talend.core.model.repository.ERepositoryObjectType;
-import org.talend.core.repository.model.ProxyRepositoryFactory;
 import org.talend.designer.core.model.utils.emf.talendfile.ColumnType;
-import org.talend.designer.core.model.utils.emf.talendfile.ElementParameterType;
 import org.talend.designer.core.model.utils.emf.talendfile.MetadataType;
 import org.talend.designer.core.model.utils.emf.talendfile.NodeType;
-import org.talend.designer.core.model.utils.emf.talendfile.ParametersType;
 import org.talend.designer.core.model.utils.emf.talendfile.ProcessType;
-import org.talend.migration.IMigrationTask.ExecutionResult;
+import org.talend.repository.model.migration.SetReadByJSONPathToFalse.AddReadByXPath;
 
 /**
  * 
  * @author kcoepeau
  * 
- * Migrate tImpalaOutput schema date pattern to use "yyyy-MM-dd"
+ * Keep Date type for component impacted by change from Date to Timestamp
  *
  */
-public class AddSchemaDatePatternUseForImpalaTask extends AbstractJobMigrationTask {
-    
+public class UseSqlDateTypeInDataframesTask extends AbstractJobMigrationTask {
+
     @Override
     public List<ERepositoryObjectType> getTypes() {
         List<ERepositoryObjectType> toReturn = new ArrayList<ERepositoryObjectType>();
-        toReturn.add(ERepositoryObjectType.PROCESS);
+        toReturn.add(ERepositoryObjectType.PROCESS_SPARK);
+        toReturn.add(ERepositoryObjectType.PROCESS_SPARKSTREAMING);
         return toReturn;
     }
 
     @Override
     public ExecutionResult execute(Item item) {
         ProcessType processType = getProcessType(item);
-        if(processType == null){
+        if (processType == null) {
             return ExecutionResult.NOTHING_TO_DO;
         }
-        
-        String componentName = "tImpalaOutput";
-        
-        try {
-            ModifyComponentsAction.searchAndModify(item, processType, new NameComponentFilter(componentName), Arrays
-                    .<IComponentConversion> asList(new IComponentConversion() {
 
-                        public void transform(NodeType node) {
-                            
-                            for(Object om : node.getMetadata()){
-                                MetadataType metadata = (MetadataType) om;
-                                for(Object oc : metadata.getColumn()){
-                                    ColumnType column = (ColumnType) oc;
-                                    if(column.getType().equals("id_Date") && column.getSourceType().equals("TIMESTAMP")){
-                                        column.setPattern("\"yyyy-MM-dd\"");
-                                    }
-                                }
-                            }
-                        }
-                        
-                    }));
+        List<String> impactedComponents =
+                Arrays.asList("tFileOutputParquet", "tHiveOutput", "tRedshiftOutput", "tSqlRow", "tMatchPairing",
+                        "tMatchPredict");
+
+        IComponentConversion addUseTimestampForDateType = new AddUseTimestampForDateType();
+
+        try {
+            for (String componentName : impactedComponents) {
+                ModifyComponentsAction.searchAndModify(item, processType, new NameComponentFilter(componentName),
+                        Arrays.<IComponentConversion> asList(addUseTimestampForDateType));
+            }
             return ExecutionResult.SUCCESS_NO_ALERT;
         } catch (Exception e) {
             return ExecutionResult.FAILURE;
         }
     }
-    
+
+    private class AddUseTimestampForDateType implements IComponentConversion {
+
+        private String field = "CHECK"; //$NON-NLS-1$
+
+        private String name = "DATE_TO_TIMESTAMP_DF_TYPE_SUBSTITUTION"; //$NON-NLS-1$
+
+        public void transform(NodeType node) {
+
+            if (ComponentUtilities.getNodeProperty(node, name) == null) {
+                ComponentUtilities.addNodeProperty(node, name, field);
+                ComponentUtilities.setNodeValue(node, name, "false");
+            }
+
+        }
+    }
+
     @Override
     public Date getOrder() {
-        GregorianCalendar gc = new GregorianCalendar(2018, 1, 18, 18, 0, 0);
+        GregorianCalendar gc = new GregorianCalendar(2018, 8, 10, 18, 0, 0);
         return gc.getTime();
     }
 

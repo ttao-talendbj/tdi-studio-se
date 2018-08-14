c5ba539e5e0494c644e81d3c08060f5bf40594c5
Anas KHABALI <akhabali@talend.com>
2018-08-14 13:31:11 +0100

    WIP - add version to metadata properties

--- main/plugins/org.talend.sdk.component.studio-integration/src/main/java/org/talend/sdk/component/studio/metadata/model/TaCoKitConfigurationModel.java
+++ main/plugins/org.talend.sdk.component.studio-integration/src/main/java/org/talend/sdk/component/studio/metadata/model/TaCoKitConfigurationModel.java
@@ -21,7 +21,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -38,23 +37,23 @@ import org.talend.sdk.component.studio.util.TaCoKitUtil;
 
 /**
  * DOC cmeng class global comment. Detailled comment
- * 
+ *
  * Provides convenient API for updating {@link Connection} properties
  */
 public class TaCoKitConfigurationModel {
 
     private final Connection connection;
-    
+
     private final ConfigTypeNode configType;
 
     private TaCoKitConfigurationModel parentConfigurationModel;
 
     private String parentConfigurationModelItemId;
-    
+
     public TaCoKitConfigurationModel(final Connection connection) {
         this(connection, Lookups.taCoKitCache().getConfigTypeNode(getConfigId(connection)));
     }
-    
+
     public TaCoKitConfigurationModel(final Connection connection, final ConfigTypeNode configType) {
         this.connection = connection;
         this.configType = configType;
@@ -74,30 +73,30 @@ public class TaCoKitConfigurationModel {
     private void setParentConfigurationId(final String parentId) {
         getAllProperties().put(TACOKIT_CONFIG_PARENT_ID, parentId);
     }
-    
+
     @SuppressWarnings("deprecation")
     private static String getConfigId(final Connection connection) {
         return (String) connection.getProperties().get(TACOKIT_CONFIG_ID);
     }
-    
+
     public static boolean isTacokit(final Connection connection) {
         return !StringUtils.isEmpty(getConfigId(connection));
     }
-    
+
     private boolean isVersionSet() {
         return getAllProperties().containsKey(TACOKIT_CONFIG_VERSION);
     }
-    
+
     public int getVersion() {
         final String version = (String) getAllProperties().get(TACOKIT_CONFIG_VERSION);
         return Integer.parseInt(version);
     }
-    
+
     @SuppressWarnings("unchecked")
     private void setVersion(final int version) {
         getAllProperties().put(TACOKIT_CONFIG_VERSION, Integer.toString(version));
     }
-    
+
     public int getConfigurationVersion() {
         return configType.getVersion();
     }
@@ -137,26 +136,12 @@ public class TaCoKitConfigurationModel {
         }
         return null;
     }
-    
+
     private String getValueType(final ConfigTypeNode typeNode, final String key) throws Exception {
         return typeNode.getProperties().stream()
                 .filter(propertyDefinition -> TaCoKitUtil.equals(key, propertyDefinition.getPath()))
                 .map(SimplePropertyDefinition::getType).findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("wrong key: " + key));
-    }
-
-    public ConfigTypeNode getFirstConfigTypeNodeContains(final String key) throws Exception {
-        ConfigTypeNode configTypeNode = null;
-        TaCoKitConfigurationModel parentModel = getParentConfigurationModel();
-        if (parentModel != null) {
-            configTypeNode = parentModel.getFirstConfigTypeNodeContains(key);
-        }
-        if (configTypeNode == null) {
-            if (contains(key)) {
-                configTypeNode = getConfigTypeNode();
-            }
-        }
-        return configTypeNode;
     }
 
     private boolean contains(final String key) {
@@ -208,13 +193,12 @@ public class TaCoKitConfigurationModel {
     private Map getAllProperties() {
         return connection.getProperties();
     }
-    
+
     @SuppressWarnings({ "rawtypes", "unchecked" })
     private Map getBuiltInProperties() {
         final Map builtIn = new HashMap();
-        final Iterator it = getAllProperties().entrySet().iterator();
-        while (it.hasNext()) {
-            final Map.Entry entry = (Entry) it.next();
+        for (final Object o : getAllProperties().entrySet()) {
+            final Entry entry = (Entry) o;
             final String key = (String) entry.getKey();
             if (BuiltInKeys.isBuiltIn(key)) {
                 builtIn.put(key, entry.getValue());
@@ -222,18 +206,17 @@ public class TaCoKitConfigurationModel {
         }
         return builtIn;
     }
-    
+
     /**
      * Returns properties view, which doesn't include built-in (service) properties
-     * 
+     *
      * @return properties view
      */
     @SuppressWarnings("rawtypes")
     public Map<String, String> getProperties() {
         Map<String, String> properties = new HashMap<>();
-        final Iterator it = getAllProperties().entrySet().iterator();
-        while (it.hasNext()) {
-            final Map.Entry entry = (Entry) it.next();
+        for (final Object o : getAllProperties().entrySet()) {
+            final Entry entry = (Entry) o;
             final String key = (String) entry.getKey();
             if (!BuiltInKeys.isBuiltIn(key)) {
                 properties.put(key, (String) entry.getValue());
@@ -241,7 +224,7 @@ public class TaCoKitConfigurationModel {
         }
         return Collections.unmodifiableMap(properties);
     }
-    
+
     @SuppressWarnings({ "rawtypes", "unchecked" })
     private void clear() {
         final Map builtIn = getBuiltInProperties();
@@ -252,20 +235,20 @@ public class TaCoKitConfigurationModel {
     public Connection getConnection() {
         return connection;
     }
-    
+
     @SuppressWarnings("unchecked")
     public void migrate(final Map<String, String> migratedProperties) {
         clear();
         getAllProperties().putAll(migratedProperties);
         setVersion(getConfigurationVersion());
     }
-    
+
     /**
      * Checks whether Configuration requires migration. Configuration requires
      * migration in case current Configuration version is greater than persisted
      * version. In case versions are equal migration is not required. If persisted
      * version is greater than current version, then IllegalStateException is thrown
-     * 
+     *
      * @return true, if migration is required
      */
     public boolean needsMigration() {
@@ -279,13 +262,13 @@ public class TaCoKitConfigurationModel {
     }
 
     public static class ValueModel {
-        
+
         private final TaCoKitConfigurationModel configurationModel;
 
         private final String value;
-        
+
         private final String type;
-        
+
         public ValueModel(final TaCoKitConfigurationModel configurationModel, final String value, final String type) {
             this.configurationModel = configurationModel;
             this.value = value;
@@ -299,7 +282,7 @@ public class TaCoKitConfigurationModel {
         public String getValue() {
             return this.value;
         }
-        
+
         public String getType() {
             return type;
         }
@@ -311,7 +294,7 @@ public class TaCoKitConfigurationModel {
         }
 
     }
-        
+
     public static class BuiltInKeys {
 
         static final String TACOKIT_CONFIG_ID = "__TACOKIT_CONFIG_ID"; //$NON-NLS-1$
--- main/plugins/org.talend.sdk.component.studio-integration/src/main/java/org/talend/sdk/component/studio/ui/wizard/page/TaCoKitConfigurationWizardPage.java
+++ main/plugins/org.talend.sdk.component.studio-integration/src/main/java/org/talend/sdk/component/studio/ui/wizard/page/TaCoKitConfigurationWizardPage.java
@@ -15,6 +15,7 @@ package org.talend.sdk.component.studio.ui.wizard.page;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.swt.SWT;
@@ -34,8 +35,8 @@ import org.talend.designer.core.model.components.EParameterName;
 import org.talend.designer.core.model.components.ElementParameter;
 import org.talend.designer.core.model.process.DataNode;
 import org.talend.sdk.component.server.front.model.ConfigTypeNode;
+import org.talend.sdk.component.studio.Lookups;
 import org.talend.sdk.component.studio.i18n.Messages;
-import org.talend.sdk.component.studio.metadata.model.TaCoKitConfigurationItemModel;
 import org.talend.sdk.component.studio.metadata.model.TaCoKitConfigurationModel;
 import org.talend.sdk.component.studio.model.parameter.Layout;
 import org.talend.sdk.component.studio.model.parameter.LayoutParameter;
@@ -43,6 +44,7 @@ import org.talend.sdk.component.studio.model.parameter.Metadatas;
 import org.talend.sdk.component.studio.model.parameter.PropertyNode;
 import org.talend.sdk.component.studio.model.parameter.PropertyTreeCreator;
 import org.talend.sdk.component.studio.model.parameter.SettingVisitor;
+import org.talend.sdk.component.studio.model.parameter.VersionParameter;
 import org.talend.sdk.component.studio.ui.composite.TaCoKitWizardComposite;
 import org.talend.sdk.component.studio.ui.wizard.TaCoKitConfigurationRuntimeData;
 
@@ -94,8 +96,6 @@ public class TaCoKitConfigurationWizardPage extends AbsTaCoKitWizardPage {
 
             final TaCoKitConfigurationRuntimeData runtimeData = getTaCoKitConfigurationRuntimeData();
             configurationModel = getConfigurationItemModel();
-            final boolean addContextFields = runtimeData.isAddContextFields();
-
             final ConfigTypeNode configTypeNode = runtimeData.getConfigTypeNode();
             final DummyComponent component = new DummyComponent(configTypeNode.getDisplayName());
             final DataNode node = new DataNode(component, component.getName());
@@ -112,21 +112,24 @@ public class TaCoKitConfigurationWizardPage extends AbsTaCoKitWizardPage {
             parameters.addAll(settingsCreator.getSettings());
             final ElementParameter layoutParameter = createLayoutParameter(root, form, category, element);
             parameters.add(layoutParameter);
+            //add version params
+            Map<String, ConfigTypeNode> nodes = Lookups.taCoKitCache().getConfigTypeNodeMap();
+            configTypeNode.getProperties()
+                    .stream()
+                    .filter(p -> p.getMetadata().containsKey("configurationtype::type") && p.getMetadata().containsKey("configurationtype::name"))
+                    .map(p -> new VersionParameter(node, p.getPath(),
+                            nodes.values().stream()
+                                    .filter(n -> n.getConfigurationType() != null)
+                                    .filter(n -> p.getMetadata().get("configurationtype::type").equals(n.getConfigurationType()))
+                                    .filter(n -> n.getName().equals(p.getMetadata().get("configurationtype::name")))
+                                    .findFirst()
+                                    .map(n -> String.valueOf(n.getVersion())).orElse("-1")))
+                    .forEach(p -> configurationModel.setValue(p));
             element.setElementParameters(parameters);
+            tacokitComposite = new TaCoKitWizardComposite(container, SWT.H_SCROLL | SWT.V_SCROLL | SWT.NO_FOCUS, category,
+                    element, configurationModel, true, container.getBackground());
+            tacokitComposite.setLayoutData(createMainFormData(runtimeData.isAddContextFields()));
 
-            tacokitComposite =
-                    new TaCoKitWizardComposite(container, SWT.H_SCROLL | SWT.V_SCROLL | SWT.NO_FOCUS, category,
-                            element, configurationModel, true, container.getBackground());
-            tacokitComposite.setLayoutData(createMainFormData(addContextFields));
-
-            if (addContextFields) {
-                // Composite contextParentComp = new Composite(container, SWT.NONE);
-                // contextParentComp.setLayoutData(createFooterFormData(tacokitComposite));
-                // contextParentComp.setLayout(new GridLayout());
-                // ContextComposite contextComp = addContextFields(contextParentComp);
-                // contextComp.addPropertyChangeListener(tacokitComposite);
-                // contextComp.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-            }
         } catch (Exception e) {
             throw new IllegalStateException(e);
         }
@@ -185,7 +188,9 @@ public class TaCoKitConfigurationWizardPage extends AbsTaCoKitWizardPage {
 
     @Override
     protected IStatus[] getStatuses() {
-        return Arrays.asList(super.getStatuses(), tacokitConfigStatus).toArray(new IStatus[0]);
+        List<IStatus> status = Arrays.asList(super.getStatuses());
+        status.add(tacokitConfigStatus);
+        return status.toArray(new IStatus[0]);
     }
 
 }
